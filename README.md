7675f7b926b4fadf865c442e046f1dc96e0e8a30
Lana <slomteva@outlook.com>
2020-10-31 21:22:35 +0200

    add styles to page blocks
    add some styles to page blocks

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Natours
+This landing page about group tours crated with Udemy course about sass and css animations.
+
+all animations in yhis project crated only with CSS3
+
+in project uses 
+gulp
+gulp browser-sync
+scss
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task ('style', function() {
+    return gulp.src ('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.+scss', gulp.series('style'));
+    
+  });
+
+gulp.task('default', gulp.parallel('browser-sync', 'style', 'watch'));
